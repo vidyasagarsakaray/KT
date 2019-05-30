b15e56160b30a5f3133040125e26788734a13c45
vidyasagarsakaray <vidyasagarsakaray@gmail.com>
2019-05-30 19:30:38 +0530

    C2

--- Scripts/TC2/Script1559224787242.groovy
+++ Scripts/TC2/Script1559224787242.groovy
@@ -13,6 +13,4 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.comment('C1')
-
-WebUI.comment('C2')
+WebUI.comment('TC2')
